5720afb559bc2af8cbf581dc709c09553b542419
Jay Klehr <jay@diablomedia.com>
2021-03-13 01:16:39 -0700

    moving to github actions and phpunit 8

--- tests/Zend/ValidateTest.php
+++ tests/Zend/ValidateTest.php
@@ -43,7 +43,7 @@ class Zend_ValidateTest extends PHPUnit\Framework\TestCase
      *
      * @return void
      */
-    public function setUp()
+    public function setUp(): void
     {
         $this->_validator = new Zend_Validate();
     }
@@ -53,7 +53,7 @@ class Zend_ValidateTest extends PHPUnit\Framework\TestCase
      *
      * @return void
      */
-    public function tearDown()
+    public function tearDown(): void
     {
         Zend_Validate::setDefaultNamespaces(array());
     }
